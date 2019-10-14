1f84c660c4aaf1ef08492dc6f3599d6d1e7f2479
Mohamed naser <nc_m.naser@hotmail.com>
2019-10-14 21:26:36 +0200

    fix w#c comments

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@
 var gulp = require("gulp");
 var browserSync = require("browser-sync").create();
 var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
 // Compile sass into CSS & auto-inject into browsers
 gulp.task("sass", function() {
   return gulp
     .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
     .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+  }))
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
